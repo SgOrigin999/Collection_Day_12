7bea292ed6e878c098cc23e4fe4ac2a01eea6c3a
SgOrigin999 <you@example.com>
2021-05-28 16:00:15 +0530

    Welcome To Address Book

---
+++ com/bridgelab/collection/AddressBook.java
@@ -0,0 +1,8 @@
+package com.bridgelab.collection;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program Using Collection::");
+	}
+}
